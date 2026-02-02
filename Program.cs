abffa6a4a66a5abd470577e331418e32d1030075
Patricia <patyfb04@gmail.com>
2026-02-02 14:50:44 -0500

    change console log

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 var destinations = new List<String> { "Paris", "New York", "Tokyo" };
 var trending = new Random().Next(0, 3);
-Console.WriteLine($"Trending destination: {destinations[trending]}");
+Console.WriteLine($"Trending destination => {destinations[trending]}");
 
